61dfae8c88827fa3a4eb945f30f8a4aa67aa4359
James Luo <contact@jamesckluo.ca>
2022-04-16 22:10:10 -0400

    Initialize

--- src/main/java/com/example/CJV805_API/Users/User.java
+++ src/main/java/com/example/CJV805_API/Users/User.java
@@ -1,4 +1,67 @@
 package com.example.CJV805_API.Users;
 
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Document("users")
 public class User {
+
+    @Id
+    private String _id;
+    private String email;
+    private String passwordHash;
+    private String firstName;
+    private String lastName;
+
+    public User() {
+    }
+
+    public User(String email, String passwordHash, String firstName, String lastName) {
+        this.email = email;
+        this.passwordHash = passwordHash;
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPasswordHash() {
+        return passwordHash;
+    }
+
+    public void setPasswordHash(String passwordHash) {
+        this.passwordHash = passwordHash;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "email='" + email + '\'' +
+                ", passwordHash='" + passwordHash + '\'' +
+                ", firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                '}';
+    }
 }
