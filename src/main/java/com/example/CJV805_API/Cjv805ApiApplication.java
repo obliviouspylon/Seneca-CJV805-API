85b702ebcce8f37d32bf813b7c697007ece1a017
James Luo <contact@jamesckluo.ca>
2022-04-20 02:28:08 -0400

    Added CORS

--- src/main/java/com/example/CJV805_API/Cjv805ApiApplication.java
+++ src/main/java/com/example/CJV805_API/Cjv805ApiApplication.java
@@ -2,11 +2,26 @@ package com.example.CJV805_API;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import java.util.List;
 
 @SpringBootApplication
 public class Cjv805ApiApplication {
+
+    @Configuration
+    @EnableWebMvc
+    public class WebConfig implements WebMvcConfigurer {
+
+        @Override
+        public void addCorsMappings(CorsRegistry registry) {
+            registry.addMapping("/**").allowedOrigins("http://localhost:3000/");
+            registry.addMapping("/**").allowedOrigins("https://bright-otter-dc93cd.netlify.app/");
+        }
+    }
 
     public static void main(String[] args) {
 
